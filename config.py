094c9241caa59aba2f8122c11caccfe73dd35fbf
Shreyas T <97907864+Shreyas-ITB@users.noreply.github.com>
2024-02-25 22:58:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 # Configure them as per your needs!
 
-TO_DATE = 200000000000  # Top date bound in epoch time
-FROM_DATE = 1546300800  # Bottom date bound in epoch time
 FILENAME_OUT = "coinbase_records.csv"  # Name of file to be imported to cointracking
-FILENAME_IN = "txs.txt"  # Output from listtransactions api call pointed towards txt file
-PRICE_FILE_NAME = 'prices.txt'  # Name of the JSON file that prices are fetched and saved to
+CURRENCY = "usd" # Name of your local currency that you want the price to be displayed in (lowercase)
+ADDRESS = "YOUR ADDRESS" # The address that you want to fetch the transactions from
