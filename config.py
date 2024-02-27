c81a3f390ff59cf00370f8e58afb0a0201bd2d6d
Shreyas T <97907864+Shreyas-ITB@users.noreply.github.com>
2024-02-27 22:57:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 
 FILENAME_OUT = "coinbase_records.csv"  # Name of file to be imported to cointracking
 CURRENCY = "usd" # Name of your local currency that you want the price to be displayed in (lowercase)
-ADDRESS = "YOUR ADDRESS" # The address that you want to fetch the transactions from
+ADDRESS = "YOUR VERUS ADDRESS" # The address that you want to fetch the transactions from
+BLOCK=2900000 # This is the block height from where your search for the transactions begin..
